3d31c85748ba6598e8da9a3a87d5335dcbba217f
Salwa Al-Kuwari <skkuwari@andrew.cmu.edu>
2025-11-11 19:35:30 +0000

    P4: Added working translator endpoint + venv setup

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
+from flask import Flask, request, jsonify
 import os
-
-from flask import Flask
-from flask import request, jsonify
 from src.translator import translate_content
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def translator():
-    content = request.args.get("content", default = "", type = str)
+    content = request.args.get("content", default="", type=str)
     is_english, translated_content = translate_content(content)
+
     return jsonify({
         "is_english": is_english,
         "translated_content": translated_content,
     })
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
